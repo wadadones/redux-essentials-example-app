fcb11eb92b2c156115b57f34be30fe84f56b275e
wadadones <jones3429@outlook.jp>
2021-01-03 18:28:03 +0900

    add initial values of posts to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
+import postsReducer from '../features/posts/postsSlice';
+
 export default configureStore({
-  reducer: () => ({}),
+  reducer: {
+    posts: postsReducer // 自分で作ったreducerをここに登録する
+    // state.postsはpostsReducerがdispatchされることでupdateされることを示している
+    // 
+  }
 })
